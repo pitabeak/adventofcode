d2ae86fd0e0de03021be88e3c2772f6405fca77d
pitabeak <pitabeak@gmail.com>
2024-12-18 16:10:25 -0500

    Add files via upload

--- rust/_24/src/days/_18.rs
+++ rust/_24/src/days/_18.rs
@@ -5,6 +5,7 @@ use std::collections::hash_map::Entry;
 
 pub fn solve(f:Box<dyn BufRead>) -> (String,String) {
 	let mut da = HashSet::new();
+	let mut pt = HashSet::new();
 	let wd = 70;
 	let fr = 1024;
 	let mut z = 0;
@@ -15,7 +16,7 @@ pub fn solve(f:Box<dyn BufRead>) -> (String,String) {
 		let x:usize = x.parse().unwrap();
 		let y:usize = y.parse().unwrap();
 		da.insert((x,y));
-		if j < fr { continue; }
+		if j < fr || !(j == fr || pt.contains(&(x,y))) { continue; }
 		let mut ds = HashMap::new();
 		let mut qu = BinaryHeap::new();
 		qu.push(Reverse((0,(0,0))));
@@ -59,8 +60,25 @@ pub fn solve(f:Box<dyn BufRead>) -> (String,String) {
 			}
 		}
 		let np = ds.get(&(wd,wd));
-		if j == fr { z = *np.unwrap(); }
-		else if np.is_none() { z2 = format!("{x},{y}"); break; }
+		if np.is_none() {
+			z2 = format!("{x},{y}");
+			break;
+		}
+		let mut n = *np.unwrap();
+		z = n;
+		let mut u = wd;
+		let mut v = wd;
+		pt.clear();
+		loop {
+			pt.insert((u,v));
+			if n == 0 { break; }
+			n -= 1;
+			if u > 0 && ds.get(&(u-1,v)).is_some_and(|&x| x==n) { u -= 1; }
+			else if v > 0 && ds.get(&(u,v-1)).is_some_and(|&x| x==n) { v -= 1; }
+			else if u < wd && ds.get(&(u+1,v)).is_some_and(|&x| x==n) { u += 1; }
+			else if v < wd && ds.get(&(u,v+1)).is_some_and(|&x| x==n) { v += 1; }
+			else { panic!(); }
+		}
 	}
 	(z.to_string(),z2)
 }
