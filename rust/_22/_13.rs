1ec85832d756b3372fd7038d587bcb0f3443e757
pitabeak <pitabeak@gmail.com>
2025-01-02 15:27:16 -0500

    Add files via upload

--- rust/_22/_13.rs
+++ rust/_22/_13.rs
@@ -1,6 +1,61 @@
 use std::io::BufRead;
-use std::fmt;
+use serde_json::Value;
+use std::cmp::Ordering;
 
+fn compare(a:&Value,b:&Value) -> Ordering {
+	match a {
+		Value::Number(x) => {
+			match b {
+				Value::Number(y) => x.as_u64().cmp(&y.as_u64()),
+				Value::Array(_) => compare(&Value::Array(vec![a.clone()]),b),
+				_ => panic!()
+			}
+		}
+		Value::Array(p) => {
+			match b {
+				Value::Number(_) => compare(a,&Value::Array(vec![b.clone()])),
+				Value::Array(q) => {
+					for i in 0..p.len().min(q.len()) {
+						let z = compare(&p[i],&q[i]);
+						if z.is_ne() { return z; }
+					}
+					p.len().cmp(&q.len())
+				}
+				_ => panic!()
+			}
+		}
+		_ => panic!()
+	}
+}
+
+pub fn solve(f:Box<dyn BufRead>) -> (String,String) {
+	let mut z = 0;
+	let mut a = Vec::new();
+	for s in f.lines() {
+		let s = s.unwrap();
+		if !s.is_empty() {
+			a.push(serde_json::from_str::<Value>(&s).unwrap());
+			let i = a.len();
+			if i%2==0 && compare(&a[i-2],&a[i-1]).is_lt() {
+				z += i/2;
+			}
+		}
+	}
+	let x = serde_json::from_str::<Value>("[[2]]").unwrap();
+	let y = serde_json::from_str::<Value>("[[6]]").unwrap();
+	a.push(x.clone());
+	a.push(y.clone());
+	a.sort_by(|a,b| compare(&a,&b));
+	let z2 = (a.iter().position(|t| t==&x).unwrap() + 1) * (a.iter().position(|t| t==&y).unwrap() + 1);
+	(z.to_string(),z2.to_string())
+}
+
+/*
+use std::io::BufRead;
+use std::fmt;
+use std::cmp::{Ordering,Ord,PartialOrd,Eq,PartialEq};
+
+#[derive(Clone)]
 enum Value {
 	Number(u8),
 	List(Vec<Value>)
@@ -14,9 +69,9 @@ impl fmt::Debug for Value {
 				if a.is_empty() {
 					write!(f,"[]")
 				} else {
-					write!(f,"[{:?}",a[0]);
+					write!(f,"[{:?}",a[0]).unwrap();
 					for x in a.iter().skip(1) {
-						write!(f,",{x:?}");
+						write!(f,",{x:?}").unwrap();
 					}
 					write!(f,"]")
 				}
@@ -25,12 +80,51 @@ impl fmt::Debug for Value {
 	}
 }
 
+impl Ord for Value {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+		match self {
+			Value::Number(x) => {
+				match other {
+					Value::Number(y) => x.cmp(y),
+					Value::List(_) => Value::List(vec![self.clone()]).cmp(other)
+				}
+			}
+			Value::List(a) => {
+				match other {
+					Value::Number(_) => self.cmp(&Value::List(vec![other.clone()])),
+					Value::List(b) => {
+						for i in 0..a.len().min(b.len()) {
+							let z = a[i].cmp(&b[i]);
+							if z.is_ne() { return z; }
+						}
+						a.len().cmp(&b.len())
+					}
+				}
+			}
+		}
+    }
+}
+
+impl PartialOrd for Value {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Value {
+    fn eq(&self, other: &Self) -> bool {
+		self.cmp(other).is_eq()
+    }
+}
+
+impl Eq for Value {}
+
 fn read_value(s:&str) -> Value {
 	let mut it = s.chars();
 	let mut c = it.next();
 	let mut st = Vec::new();
 	loop {
-		let mut v =
+		let v =
 		if Some('[') == c {
 			c = it.next();
 			if Some(']') == c {} else { st.push(Vec::new()); continue; }
@@ -39,8 +133,9 @@ fn read_value(s:&str) -> Value {
 		} else {
 			let mut x = 0;
 			while let Some(d) = c {
-				x = x*10 + d::parse<u8().unwrap();
+				let d = d as u8;
 				if !(b'0'<=d && d<=b'9') { break; }
+				x = x*10 + (d - b'0');
 				c = it.next();
 			}
 			Value::Number(x)
@@ -55,15 +150,30 @@ fn read_value(s:&str) -> Value {
 			let l = st.len();
 			st[l-1].push(v);
 		}
-		assert!(Some(b',') == c);
+		assert!(Some(',') == c);
 		c = it.next();
 	}
 }
 
 pub fn solve(f:Box<dyn BufRead>) -> (String,String) {
+	let mut z = 0;
+	let mut a = Vec::new();
 	for s in f.lines() {
 		let s = s.unwrap();
-		if s.len() > 0 { println!("{:?}",read_value(&s.as_bytes())); }
+		if !s.is_empty() {
+			a.push(read_value(&s));
+			let i = a.len();
+			if i%2==0 && a[i-2] < a[i-1] {
+				z += i/2;
+			}
+		}
 	}
-	(0.to_string(),0.to_string())
+	let x = read_value("[[2]]");
+	let y = read_value("[[6]]");
+	a.push(x.clone());
+	a.push(y.clone());
+	a.sort();
+	let z2 = (a.iter().position(|t| t==&x).unwrap() + 1) * (a.iter().position(|t| t==&y).unwrap() + 1);
+	(z.to_string(),z2.to_string())
 }
+*/
